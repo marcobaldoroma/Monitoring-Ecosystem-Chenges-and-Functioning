5b4a2e93010fe3c33aa91e936724e3ae8768950c
Marco Baldo <62661915+marcobaldoroma@users.noreply.github.com>
2020-05-14 04:03:46 +0200

    Update R_code_faPAR.r

--- R_code_faPAR.r
+++ R_code_faPAR.r
@@ -1,21 +1,21 @@
 # how to look energy flow and CO2 cycle from satellite: faPAR fraction of absorbed photosynthetically active radiation
 
-library(raster)                                            # install.packages("raster")
-library(rasterVis)                                         # for levelplot
+library(raster)                                             # install.packages("raster")
+library(rasterVis)                                          # for levelplot
 library(rasterdiv)
 
-                                                           #copDVI= copernicus DIV = World wide analysis DVI different variation index
+                                                            #copDVI= copernicus DIV = World wide analysis DVI different variation index
 setwd("C:/lab/")
 plot(copNDVI)
 
-copNDVI <- reclassify(copNDVI, cbind(253:255, NA))         #reclissifaty coppenNDVI   remuving data from 255-255 and putting No Data= NA
-levelplot(copNDVI)                                         #levelplot cran
-                                                           #different vegetation index... you can make an avarage of the pixel variable value, very high NDVI are in primary equator forest and broad-laef and conifer northern forest
+copNDVI <- reclassify(copNDVI, cbind(253:255, NA))          #reclissifaty coppenNDVI   remuving data from 255-255 and putting No Data= NA
+levelplot(copNDVI)                                          #levelplot cran
+                                                            #different vegetation index... you can make an avarage of the pixel variable value, very high NDVI are in primary equator forest and broad-laef and conifer northern forest
 
-faPAR10 <- raster("faPAR10.tif")                           #give name 10 per pixel aggregation 10=1 ,   raster = data   faPAR10= name of image
+faPAR10 <- raster("faPAR10.tif")                            #give name 10 per pixel aggregation 10=1 ,   raster = data   faPAR10= name of image
 levelplot(faPAR10)
 
-pdf("copNDVI.pdf")                                         #make a pdf for DVI and faPAR
+pdf("copNDVI.pdf")                                          #make a pdf for DVI and faPAR
 levelplot(copNDVI)
 dev.off()
 
