69beb8f070a8756332427dc4620ffd85e842415a
Marco Baldo <62661915+marcobaldoroma@users.noreply.github.com>
2020-05-14 04:16:00 +0200

    Update R_code_radiance.r

--- R_code_radiance.r
+++ R_code_radiance.r
@@ -1,30 +1,28 @@
 # R_code_radiance
 
-library(raster)
+library(raster) 
 
-# create a new raster dataset 
-
-toy <- raster(ncol=2, nrow=2, xmn=1, xmx=2, ymn=1, ymx=2)
+toy <- raster(ncol=2, nrow=2, xmn=1, xmx=2, ymn=1, ymx=2)              # create a new raster dataset
 values(toy) <- c(1.13,1.44,1.55,3.4)
 
 plot(toy)
-text(toy, digits=2)      # put the information on the pixel
+text(toy, digits=2)                                                    # put the information on the pixel
 
-toy2bits <- stretch(toy,minv=0,maxv=3)    # we traslate the information = radiance in bits information
+toy2bits <- stretch(toy,minv=0,maxv=3)                                 # we traslate the information = radiance in bits information
 
-storage.mode(toy2bits[]) = "integer"      # the way to starage our information 
+storage.mode(toy2bits[]) = "integer"                                   # the way to starage our information 
 
 plot(toy2bits)
 text(toy2bits, digits=2)
 
-toy4bits <- stretch(toy,minv=0,maxv=15)     # we can change the information from 2bits to 4bits
+toy4bits <- stretch(toy,minv=0,maxv=15)                                 # we can change the information from 2bits to 4bits
  
 
 storage.mode(toy4bits[]) = "integer"
- 
+
 plot(toy4bits)
 text(toy4bits, digits=2)
-                                # passing from 4 bits to 8
+                                                                        # passing from 4 bits to 8
                                                        
                                 
 toy8bits <- stretch(toy,minv=0,maxv=255)
@@ -38,33 +36,11 @@ text(toy8bits, digits=2)
                                 
 par(mfrow=c(1,4))  
 
-### second exercise 
-
-setwd("C:/lab/")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                
-                                
+plot(toy)
+text(toy, digits=2)
+plot(toy2bits)
+text(toy2bits, digits=2)
+plot(toy4bits)
+text(toy4bits, digits=2)
+plot(toy8bits)
+text(toy8bits, digits=2)
