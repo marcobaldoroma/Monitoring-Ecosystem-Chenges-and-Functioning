df03b3f9ee8edecce759acb68e8114d999c0bbe3
Marco Baldo <62661915+marcobaldoroma@users.noreply.github.com>
2020-05-15 05:01:38 +0200

    Update R_code_multipanel.r

--- R_code_multipanel.r
+++ R_code_multipanel.r
@@ -5,7 +5,6 @@ install.packages("GGally")                       # this is required to use the f
 
                                                  #require those library
 library(sp)
-library(GGally)
 
 data(meuse)                                      # there is a dataset available named meuse
 
@@ -28,7 +27,7 @@ plot(x,copper)
                                                  # plot(cadmium,zinc,pch=15,col="red",cex=2) an amateur exercise to prettify plots
                                                  # plot is not a good idea sometimes
 pairs(meuse)
-                                                 #selection of variables we want to plot together
+                                                 # selection of variables we want to plot together
 pairs(~ cadmium + copper + lead + zinc, data=meuse)
 
 pairs(meuse[,3:6])
@@ -37,5 +36,13 @@ pairs(meuse[,3:6])
 pairs(meuse[,3:6], col="red", pch=18, cex=1.5)
 
 
-                                                 # GGally package will show infographic even better
+                                                 
 pairs(meuse[,3:6])
+
+                                                 # from 3(cadmium) to 6(zinc)
+                                                 # 16 and 15 are the same function!
+
+pairs(meuse[,3:6],pch=19)
+
+library(GGally)                                  # GGally package will show infographic even better
+ggpairs(meuse[,3:6])
